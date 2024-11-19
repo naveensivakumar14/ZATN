719802f773106315c7b649a7467b7e51270c2114
Naveen_Sivakumar <45707585+naveensivakumar14@users.noreply.github.com>
2024-11-19 21:28:30 +0530

    Add files via upload

---
+++ class 5/practice.js
@@ -0,0 +1,64 @@
+
+
+const http = require('http')
+const fs =  require('fs')
+
+
+const server = http.createServer((req,res)=>{
+    if(req.url=='/favicon.ico') return ;
+
+    const data = `${Date.now()} : request made to ${req.url}\n`
+
+    fs.appendFile('myPage.txt',data,(err)=>{
+        if(err){
+            console.log(err)
+        }
+        switch(req.url){
+            case '/':
+                res.end("Hi Welcome to my Portfolio Naveenkumar")
+                break;
+            case '/home':
+                res.end("Welcome to Home page")
+                break;
+
+            case '/about':
+                res.end("I am a Backend developer")
+                break;
+            
+            case '/skills':
+                res.end("MERN STACK DEVELOPER")
+                break;
+            
+            case '/project':
+                res.end("Weather APP")
+                break;
+            
+            case '/contact':
+                res.end("naveen123@gmail.com")
+                break;
+            
+            default:
+                res.end("you are not in the wesbite")
+                break;
+        }
+    })  
+})
+server.listen(3000,()=>{
+    console.log("server is listening...")
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
